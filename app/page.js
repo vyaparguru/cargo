f700063f4d68993f2e81fc78d9ee28c1d5cad816
relish <guptarelish333@gmail.com>
2025-10-14 11:26:35 +0530

    added under const

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
-import UnderConstruction from "@/components/underconst";
+import UnderConstruction from "@/components/Underconst";
 import WhyUs from "@/components/Whyus";
 
 export default function Home() {
